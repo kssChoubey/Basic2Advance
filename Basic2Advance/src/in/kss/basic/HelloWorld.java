44fe85e38138bb16be3824d0212edc52e0380187
kssChoubey <krishna0choubey@gmail.com>
2025-07-06 12:21:38 +0530

    check

--- Basic2Advance/src/in/kss/basic/HelloWorld.java
+++ Basic2Advance/src/in/kss/basic/HelloWorld.java
@@ -3,6 +3,8 @@ package in.kss.basic;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello CHeck!");
+		System.out.println("Hello World!");
 	}
 
 }
