1931e7692981adf0e2d2fd874504623ad480e235
Krishna <Krishna@DESKTOP-ITC5PRU>
2025-12-02 21:58:07 +0530

    Check

--- Basic2Advance/src/in/kss/basic/HelloWorld.java
+++ Basic2Advance/src/in/kss/basic/HelloWorld.java
@@ -2,6 +2,6 @@ package in.kss.basic;
 //Write a Java program to print Hello in Java.
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World.!");
 	}
 }
